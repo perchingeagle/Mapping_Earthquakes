81775ba2307181f561693d688b8918936b91ca7c
perchingeagle <perchingeagle@gmail.com>
2021-11-13 23:16:38 -0500

    Polyline added

--- static/js/logic.js
+++ static/js/logic.js
@@ -3,6 +3,8 @@ console.log(API_KEY);
 
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
+// let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
 const mapStyle = {
     street: 'mapbox/streets-v11',
     outdoors: 'mapbox/outdoors-v11',
@@ -24,24 +26,43 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
-// L.circle([34.0522, -118.2437], {
-//     radius: 100
-//  }).addTo(map);
+//  cities.forEach(city => {
+//     console.log(city);
+//     L.circleMarker(city.location, { radius: city.population/200000})
+//     .bindPopup(`<h2>${city.city}, ${city.state}</h2><hr/><h3>Population ${city.population.toLocaleString('en')}</h3>`)
+//     .addTo(map);
+// });
 
-//  L.circleMarker([34.0522, -118.2437]).addTo(map);
+// Coordinates for each point to be used in the line.
+// let line = [
+//     [33.9416, -118.4085],
+//     [37.6213, -122.3790]
+//   ];
 
- // An array containing each city's location, state, and population.
+// let line = [
+//     [33.9416, -118.4085],
+//     [37.6213, -122.3790],
+//     [40.7899, -111.9791],
+//     [47.4502, -122.3088]
+//   ];
 
- cities.forEach(city => {
-    console.log(city);
-    L.circleMarker(city.location, { radius: city.population/200000})
-    .bindPopup(`<h2>${city.city}, ${city.state}</h2><hr/><h3>Population ${city.population.toLocaleString('en')}</h3>`)
-    .addTo(map);
-});
+const airports = {
+    lax: [33.9416, -118.4085],
+    aus: [30.1975, -91.6664],
+    yyz: [43.6777, -79.6248],
+    jfk: [40.6413, -73.7781]
+};
+
+let line = [
+    airports.lax,
+    airports.aus,
+    airports.yyz,
+    airports.jfk
+];
 
 
-
-
-// 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}'
+L.polyline(line, {
+    color: "yellow",
+    dashArray: '5, 10'
+  }).addTo(map);
