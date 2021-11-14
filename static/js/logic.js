d8d7df734df09abd06e8785eab4ef1038c4adbc7
perchingeagle <perchingeagle@gmail.com>
2021-11-14 16:28:06 -0500

    module 13.5.6

--- static/js/logic.js
+++ static/js/logic.js
@@ -51,18 +51,32 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: streets,
+    Streets: streets,
     Dark: dark,
-    Light: light
+    Light: light,
+    "Satellite Streets": satelliteStreets
   };
 
   // Create the map object with center, zoom level and default layer.
+// let map = L.map('mapid', {
+//     center: [30, 30],
+//     zoom: 2,
+//     layers: [light]
+// })
+
 let map = L.map('mapid', {
-    center: [30, 30],
-    zoom: 2,
-    layers: [light]
+    center: [43.68108112399995, -79.39119482699992],
+    zoom: 11,
+    layers: [satelliteStreets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -136,9 +150,11 @@ L.control.layers(baseMaps).addTo(map);
 // L.geoJSON(sanFranAirport).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/perchingeagle/Mapping_Earthquakes/main/majorAirports.json";
+//let airportData = "https://raw.githubusercontent.com/perchingeagle/Mapping_Earthquakes/main/majorAirports.json";
 
-let torontoData = "https://raw.githubusercontent.com/perchingeagle/Mapping_Earthquakes/main/torontoRoutes.json";
+// let torontoData = "https://raw.githubusercontent.com/perchingeagle/Mapping_Earthquakes/main/torontoRoutes.json";
+
+let torontoHoods = "https://raw.githubusercontent.com/perchingeagle/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
 // Grabbing our GeoJSON data.
 // d3.json(airportData).then(function(data) {
@@ -153,14 +169,27 @@ let torontoData = "https://raw.githubusercontent.com/perchingeagle/Mapping_Earth
 //   }).addTo(map);
 // });
 
-d3.json(torontoData).then(function(data) {
-    console.log(data);
+// d3.json(torontoData).then(function(data) {
+//     console.log(data);
+//   // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJson(data, {
+//       pointToLayer:function(feature, latlng){
+//           return L.marker(latlng)
+//                   .bindPopup(`<h3>Airline: ${feature.properties.airline}</h3>
+//                   <hr/><h4>Destination: ${feature.properties.dst}</h4>`);
+//       }
+//   }).addTo(map);
+// });
+
+d3.json(torontoHoods).then(function(data) {
+    //console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data, {
       pointToLayer:function(feature, latlng){
+          console.log(feature);
           return L.marker(latlng)
-                  .bindPopup(`<h3>Airline: ${feature.properties.airline}</h3>
-                  <hr/><h4>Destination: ${feature.properties.dst}</h4>`);
+                  .bindPopup(`<h3>Neighborhood: ${feature.properties.AREA_NAME}</h3>
+                  <hr/>${feature.properties.AREA_S_CD}</h4>`);
       }
   }).addTo(map);
 });
