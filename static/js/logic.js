bcbd24778fc10102d67c7b98ed7f1383942e2a9e
perchingeagle <perchingeagle@gmail.com>
2021-11-13 22:44:26 -0500

    cities.js added

--- static/js/logic.js
+++ static/js/logic.js
@@ -16,7 +16,7 @@ const mapStyle = {
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: mapStyle.street,
+    id: mapStyle.dark,
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
@@ -24,6 +24,24 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// L.circle([34.0522, -118.2437], {
+//     radius: 100
+//  }).addTo(map);
+
+//  L.circleMarker([34.0522, -118.2437]).addTo(map);
+
+ // An array containing each city's location, state, and population.
+
+ cities.forEach(city => {
+    console.log(city);
+    L.circleMarker(city.location, { radius: city.population/200000})
+    .bindPopup(`<h2>${city.city}, ${city.state}</h2><hr/><h3>Population ${city.population.toLocaleString('en')}</h3>`)
+    .addTo(map);
+});
+
+
+
 
 // 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}'
